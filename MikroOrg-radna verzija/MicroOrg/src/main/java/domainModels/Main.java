3e2941fe6283551fe69add404536ed776ca97444
Arnela Tumbul <atumbul1@etf.unsa.ba>
2014-05-31 22:55:32 +0200

    izbrisana konzolna pretraga u domainModels.Main.java

--- MikroOrg-radna verzija/MicroOrg/src/main/java/domainModels/Main.java
+++ MikroOrg-radna verzija/MicroOrg/src/main/java/domainModels/Main.java
@@ -18,7 +18,7 @@ public class Main {
  public static void main(String[] args) {Session session =   HibernateUtil.getSessionFactory().openSession();
 
  dodajOsobu(session);
- nadjiOsobu(session); 
+  
  session.close(); 
  } 
  
@@ -67,29 +67,5 @@ Uposlenik u= new Uposlenik();
  t.commit(); 
  } 
  
- private static void nadjiOsobu(Session session) { 
- Transaction t = session.beginTransaction(); 
- 
- System.out.println("Unesite ime osobe"); 
- String _ime = sc.next();
- Criteria criteria = session.createCriteria(Osoba.class);
- Osoba _o =(Osoba) criteria.add(Restrictions.eq("imePrezime", _ime)).uniqueResult();
-                              
- if (_o==null) { 
- System.out.println("Nema osobe sa tim imenom u bazi"); 
- } else { 
- System.out.println("Student: "+_o.getImePrezime()+" "+_o.getEmail()); 
- } 
- 
- /*System.out.println("Unesite id osobe"); 
- long id = sc.nextLong(); 
- Osoba o = (Osoba) session.get(Osoba.class, id); 
- if (o==null) { 
- System.out.println("Nema osobe sa tim IDom u bazi"); 
- } else { 
- System.out.println("Student: "+o.getImePrezime()+" "+o.getEmail()); 
- } */
- 
- t.commit(); 
- } 
+
 } 
