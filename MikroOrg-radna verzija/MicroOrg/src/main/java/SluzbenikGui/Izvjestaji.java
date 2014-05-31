dfb1d555f2b57fb53a6fa480f42d3dbf90f66219
hamdijasinanovic <hsinanovic1@etf.unsa.ba>
2014-05-31 10:44:18 +0200

    implementacija forme

--- MikroOrg-radna verzija/MicroOrg/src/main/java/SluzbenikGui/Izvjestaji.java
+++ MikroOrg-radna verzija/MicroOrg/src/main/java/SluzbenikGui/Izvjestaji.java
@@ -18,13 +18,29 @@ import javax.swing.border.LineBorder;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import viewModels.IzvjestajSluzbenika;
 import aplikacija.MicroOrg.Spremnik;
+import domainModels.HibernateUtil;
+import domainModels.Kredit;
+import domainModels.Transakcija;
 import domainModels.Uposlenik;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import logic.IzvjestajLogika;
+import logic.SharedLogika;
 
 public class Izvjestaji extends JFrame {
 
@@ -62,6 +78,51 @@ public class Izvjestaji extends JFrame {
 				n.setResizable(false);
 			}
 		});
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+	    Transaction t = session.beginTransaction();
+	    
+	    /*Query q = session.createQuery("FROM kredit");
+	    List<Kredit> listaKredita = q.list();
+	    for(Kredit _k_ : listaKredita){
+	    	if(!(_k_.getKreditniSluzbenik().equals(Spremnik.getTrenutni()))) {
+	    		listaKredita.remove(_k_);
+	    	}
+	    }*/
+	    
+	    Criteria _c = session.createCriteria(Kredit.class);
+	    List<Kredit> _krediti = (List<Kredit>)_c.add(Restrictions.eq("kreditniSluzbenik.uposlenikId", Spremnik.getTrenutni().getUposlenikId())).list();
+	    
+
+	    Criteria criteria = session.createCriteria(Transakcija.class);
+	    List<Transakcija> _transakcije = (List<Transakcija>)criteria.add(Restrictions.eq("kreditniSluzbenik.uposlenikId", Spremnik.getTrenutni().getUposlenikId())).list();
+	    
+	    /*List<Transakcija> _transakcije = (List<Transakcija>) criteria.list();
+	    for(Transakcija _t_ : _transakcije){
+	    	if(!(_t_.getKreditniSluzbenik().equals(Spremnik.getTrenutni()))) {
+	    		_transakcije.remove(_t_);
+	    	}
+	    }*/
+	    
+	    //Set<Kredit> _krediti = Spremnik.getTrenutni().getKrediti(); 
+	    Kredit[] _notSet = _krediti.toArray(new Kredit[_krediti.size()]);
+	    
+	    //Criteria criteria = session.createCriteria(Kredit.class);
+	    //List<Kredit> _krediti = (List<Kredit>) criteria.add(Restrictions.eq("uposlenikId", Spremnik.getTrenutni().getUposlenikId())).list();
+	    double _rashodi = 0;
+	    double _prihodi = 0;
+	    //for(Kredit k : _krediti) {
+	    	//_rashodi += k.getTipKredita().getIznos();
+	    for(int i = 0; i < _krediti.size(); i++) {
+	    	_rashodi += _notSet[i].getTipKredita().getIznos();
+	    }
+	    for(Transakcija _t : _transakcije) {
+	    	_prihodi += _t.getIznosUplate();
+	    }
+	    
+	    t.commit();
+	    session.close();
+		
 		trenutni=Spremnik.getTrenutni();
 		setBackground(Color.WHITE);
 		setTitle("MicroOrg - Izvje\u0161taji");
@@ -92,33 +153,50 @@ public class Izvjestaji extends JFrame {
 		label_1.setBounds(307, 39, 231, 14);
 		panel.add(label_1);
 		
-		JList list = new JList();
+		Transakcija[] listaTransakcija = new Transakcija[_transakcije.size()];
+		String[] lista = new String[_transakcije.size()];
+		for (int i = 0; i < _transakcije.size(); i++) {
+			listaTransakcija[i] = _transakcije.get(i);
+			lista[i] = Long.toString(listaTransakcija[i].getTransakcijaId()) + "    " + listaTransakcija[i].getDatumUplate() + "    " + Double.toString(listaTransakcija[i].getIznosUplate());
+		}
+				
+		//JList list = new JList(listaTransakcija);
+		JList list = new JList(lista);
 		list.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list.setBounds(307, 59, 242, 214);
+		
 		panel.add(list);
 		
-		JLabel label_2 = new JLabel("Prihodi:");
+		/*JList list = new JList();
+		list.setBorder(new LineBorder(new Color(0, 0, 0)));
+		list.setBounds(307, 59, 242, 214);
+		panel.add(list);*/
+		
+		JLabel label_2 = new JLabel("Prihodi: " + String.valueOf(_prihodi));
 		label_2.setHorizontalAlignment(SwingConstants.TRAILING);
 		label_2.setBounds(10, 100, 141, 14);
 		panel.add(label_2);
 		
-		JLabel label_3 = new JLabel("Rashodi:");
+		JLabel label_3 = new JLabel("Rashodi: " + String.valueOf(_rashodi));
 		label_3.setHorizontalAlignment(SwingConstants.TRAILING);
 		label_3.setBounds(10, 125, 141, 14);
 		panel.add(label_3);
 		
-		JLabel label_4 = new JLabel("Izvr\u0161ene transakcije:");
+		JLabel label_4 = new JLabel("Izvr\u0161ene transakcije: " + String.valueOf(Spremnik.getTrenutni().getUkupanBrTransakcija()));
 		label_4.setHorizontalAlignment(SwingConstants.TRAILING);
 		label_4.setBounds(10, 150, 141, 14);
 		panel.add(label_4);
 		
-		JLabel label_5 = new JLabel("Dodijeljeni krediti:");
+		JLabel label_5 = new JLabel("Dodijeljeni krediti: " + String.valueOf(Spremnik.getTrenutni().getUkupanBrKredita()));
 		label_5.setHorizontalAlignment(SwingConstants.TRAILING);
 		label_5.setBounds(10, 175, 141, 14);
 		panel.add(label_5);
 		
-		JComboBox comboBox = new JComboBox();
+		final JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(188, 8, 176, 20);
+		comboBox.addItem("mjesec");
+		comboBox.addItem("sest mjeseci");
+		comboBox.addItem("godina");
 		panel.add(comboBox);
 		
 		JLabel label_6 = new JLabel("Za protekli vremenski period:");
@@ -134,19 +212,50 @@ public class Izvjestaji extends JFrame {
 		JButton button = new JButton("PDF prikaz");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Nije implementirano !");
+				
+				IzvjestajLogika _i = new IzvjestajLogika();
+				IzvjestajSluzbenika izvjestaj = _i.generisiIzvjestajSluzbenika((String)comboBox.getSelectedItem());
+				try{
+					SharedLogika _sharedLogika=new SharedLogika();
+					_sharedLogika.generisiPDF(izvjestaj);
+					_sharedLogika.otvoriPDF(izvjestaj);
+						
+				}
+				catch (NullPointerException e1) {
+					JOptionPane.showMessageDialog(null, "Niste odabrali ponudu koju želite prikazati u pdf formatu!");
+				} 
+				catch (Exception e1) {
+					JOptionPane.showMessageDialog(null, "Nešto je krenulo po zlu! ERROR: pr1k4z 3rr0r");
+				}
 			}
 		});
-		button.setBounds(10, 285, 110, 23);
+		button.setBounds(10, 285, 121, 23);
 		panel.add(button);
 		
 		JButton btnEmail = new JButton("E-mail");
 		btnEmail.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Nije implementirano !");
+				
+				
+				IzvjestajLogika _i = new IzvjestajLogika();
+				IzvjestajSluzbenika izvjestaj = _i.generisiIzvjestajSluzbenika((String)comboBox.getSelectedItem());
+				try {
+						SharedLogika _sharedLogika= new SharedLogika();
+						Spremnik.setObjekatPDF(izvjestaj);
+						Mail m = new Mail();
+						m.setVisible(true);
+							
+				}
+				catch (NullPointerException e1) {
+					JOptionPane.showMessageDialog(null, "Niste odabrali klijenta čije podatke želite prikazati u pdf formatu!");
+				} 
+				catch (Exception e1) {
+					JOptionPane.showMessageDialog(null, "Nešto je krenulo po zlu! ERROR: pr1k4z 3rr0r");
+				}
+				
 			}
 		});
-		btnEmail.setBounds(142, 285, 163, 23);
+		btnEmail.setBounds(156, 285, 149, 23);
 		panel.add(btnEmail);
 		
 		JButton button_2 = new JButton("Nazad");
@@ -158,9 +267,13 @@ public class Izvjestaji extends JFrame {
 		button_2.setBounds(460, 285, 89, 23);
 		panel.add(button_2);
 		
-		JLabel label_8 = new JLabel("Datum i vrijeme:");
+		DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm");
+		Calendar cal = Calendar.getInstance();
+		String datum = dateFormat.format(cal.getTime());
+		
+		JLabel label_8 = new JLabel("Datum i vrijeme: " + datum);
 		label_8.setHorizontalAlignment(SwingConstants.TRAILING);
-		label_8.setBounds(10, 225, 141, 14);
+		label_8.setBounds(10, 225, 204, 14);
 		panel.add(label_8);
 	}
 
