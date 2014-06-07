d5e9b55285eb9a094b7b4626615f8c572a620bfe
Elvis <erudonja1@etf.unsa.ba>
2014-06-07 15:01:44 +0200

    riješen commit #79, testiranje dodajPonudu

--- MikroOrg-radna verzija/MicroOrg/src/test/java/logic/PonudeLogikaTest.java
+++ MikroOrg-radna verzija/MicroOrg/src/test/java/logic/PonudeLogikaTest.java
@@ -1,10 +1,19 @@
 package logic;
 
+import java.util.List;
+
+import viewModels.KlijentSluzbenik;
+import viewModels.KreditnaPonuda;
+import viewModels.TipKreditaSluzbenik;
+import domainModels.Klijent;
+import domainModels.Osoba;
+import domainModels.TipKredita;
 import junit.framework.TestCase;
 
 public class PonudeLogikaTest extends TestCase {
+	PonudeLogika _ponudeLogika= new PonudeLogika();
 	public void testValidirajPodatke(){
-		PonudeLogika _ponudeLogika= new PonudeLogika();
+		
 		String naziv="Nekii";
 		String namjena="Namjena";
 		String iznos="1000";
@@ -18,10 +27,66 @@ public class PonudeLogikaTest extends TestCase {
 	}
 	public void testPostojiKlijent(){
 		// Klijent k=new Klijent() nećemo dodavati novog klijenta zbog baze jer kad bi se test ponovio 2 puta desio bi se error u bazi
-		PonudeLogika _ponudeLogika= new PonudeLogika();
+		//PonudeLogika _ponudeLogika= new PonudeLogika();
 		assertEquals((Object)false, _ponudeLogika.postojiKlijent("11111", "11111")); //ne postoji klijent sa ovakvim imenom sigurno
 		assertEquals((Object)false, _ponudeLogika.postojiKlijent("12345", " "));
 		assertEquals((Object)false, _ponudeLogika.postojiKlijent("1sadasd", "habir14567"));
 		assertEquals((Object)false, _ponudeLogika.postojiKlijent("aaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbb"));
+	}
+	public void testDodajPonudu(){
+		TipKredita tip=new TipKredita();
+		KlijentSluzbenik klijent=new KlijentSluzbenik();
+		
+		KlijentLogika klijentLogika=new KlijentLogika();
+		Osoba osoba=new Osoba();
+		
+		if(klijentLogika.daLiPostoji("0000000000000") && klijentLogika.daLiPostojiEmail("testPonudeLogika"))
+		{
+		osoba=klijentLogika.getOsobaByJMBG("0000000000000");
+		List<KlijentSluzbenik> lista=klijentLogika.getByName(osoba.getImePrezime());
+		}
+		else{
+		osoba.setImePrezime("Test PonudeLogika");
+		osoba.setJmbg("0000000000000");
+		osoba.setEmail("testPonudeLogika");
+		osoba.setAdresa("Test adresa");
+		osoba.setAktivan(true);
+		osoba.setObrazovanje("Test obrazovanje");
+		
+		}
+		klijent.setAdresa(osoba.getAdresa());
+		klijent.setDatumRodjenja(osoba.getDatumRodjenja());
+		klijent.setStatus("aktivan");
+		klijent.setEmail(osoba.getEmail());
+		klijent.setImePrezime(osoba.getImePrezime());
+		klijent.setJmbg(osoba.getJmbg());
+		klijent.setTelefon(osoba.getTelefon());
+		if (klijentLogika.daLiPostoji("0000000000000")==false) {klijentLogika.dodajKlijenta(klijent);}
+		Klijent pravi=new Klijent();
+		TipKreditaLogika kreditLogika=new TipKreditaLogika();
+		TipKreditaSluzbenik tipkredita=new TipKreditaSluzbenik();
+		int tmp=0;
+		for(int i=0;i<100;i++)
+		{
+			
+			tip.setGarancija("test br"+i);
+			tip.setNaziv("Test"+i);
+			if ((_ponudeLogika.dajTipKredita("Test"+i)==null))
+			{
+				tmp=i;
+				tipkredita.setGarancija(tip.getGarancija());
+				tipkredita.setGracePeriod(tip.getGracePeriod());
+				tipkredita.setNaziv(tip.getNaziv());
+				tipkredita.setNamjena(tip.getNamjena());
+				kreditLogika.dodajTipKredita(tipkredita);
+				
+				KreditnaPonuda k=new KreditnaPonuda(null,null,null,"2014.07.06");
+				_ponudeLogika.dodajPonudu(k);
+
+				break;
+			}
+		}
+		assertNotNull(_ponudeLogika.dajTipKredita("Test"+tmp));
+		
 	}
 }
